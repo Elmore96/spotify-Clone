666a5c6a767324cad1025b08af8df64ecaca7463
elmore <eldad.ab@walla.co.il>
2022-05-24 19:06:07 +0300

    main

---
+++ src/components/log-in-comps/Submit.js
@@ -0,0 +1,10 @@
+
+export default function Submit(Props){
+    return (
+        <div className="container">
+            <lable>User Name</lable> <input type='text'></input>
+            <lable>Password</lable> <input type='text'></input>
+            <button></button>
+        </div>
+    )
+}
