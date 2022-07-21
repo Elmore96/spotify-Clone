79d3c3efd0ab3870c3278ac957e15023207ad8a2
elmore <eldad.ab@walla.co.il>
2022-07-21 13:01:23 +0300

    home desian

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,53 +7,36 @@ import './Home.css'
 
 export default function Home() {
 
-  // const [serch, setSerch] = useState('')
   const [main, setMain] = useState([])
-
-
-  // function Serching() {
-  //   const options = {
-  //     method: 'GET',
-  //     headers: {
-  //       'Authorization': `Bearer ${localStorage.getItem('token')} `,
-  //       'X-RapidAPI-Host': 'simple-youtube-search.p.rapidapi.com',
-  //       'X-RapidAPI-Key': '8fe5f15862mshd21e36bd48c59cap1d5a95jsn5c1159de872b'
-  //     }
-  //   };
-
-  //   let res = serch.split(' ').join('%2B')
-
-  //   fetch('https://simple-youtube-search.p.rapidapi.com/search?query=' + res, options)
-  //     .then(response => response.json())
-  //     .then(response => { console.log(response.results); setMain(response.results) })
-  //     .catch(err => console.error(err))
-  // }
-
 
   return (
     <div className='homePage'>
-        <Header setMain={setMain}/>
-      {/* <header className='search'>
-        <label style={{ fontSize: '1.5rem', marginTop: '-0.2rem', marginRight: '0.4rem' }}>search</label>
-        <input type='text' style={{ height: '1.5rem', fontSize: '1.3rem', fontFamily: 'serif' }} onChange={(e) => setSerch(e.target.value)} />
-        <button style={{ fontSize: '1.3rem', marginLeft: '0.4rem' }} onClick={Serching}>click</button>
-      </header> */}
+      <Header setMain={setMain}/>
+      <div className='block-container'>
       <div className='container'>
-        {main.map(v => {
-          let views = (v.views.toString().length > 6) ? v.views.toLocaleString().slice(-0, -8) + ' M' :
-            (v.views.toString().length > 3) ? v.views.toLocaleString().slice(-0, -4) + ' K' : v.views.toLocaleString();
-          let title = (v.title.length > 35) ? `${v.title.slice(0, 35)}...` : v.title;
-          return (<div key={v.id} className='card'>
-            
-            <div className='title'>{title}</div>
+        { 
+        main.map(v => {
+        let views = (v.views.toString().length > 6) ? v.views.toLocaleString().slice(-0, -8) + ' M' :
+        (v.views.toString().length > 3) ? v.views.toLocaleString().slice(-0, -4) + ' K' : v.views.toLocaleString();
+          let title = (v.title.length > 25) ? `${v.title.slice(0, 25)}...` : v.title;
+          return (<div key={v.id} className='card' data-txt={title}>
             <Link to={'/Song/' + `${v.id}`}>
-              <div className='image' style={{ backgroundImage: `url(${v.thumbnail.url})`, backgroundSize: 'cover' }} ></div>
-            </Link>
+              <div className='imgBx'>
+              <div className='image' style={{ backgroundImage: `url(${v.thumbnail.url})` }} ></div>
+              </div>
+            <div className='contentBx'>
+              <div className='bottom-popup'>
+            <div className='title' onMouseOver={(el)=> el.target.innerText = v.title}
+              onMouseLeave={(el)=> el.target.innerText = title}  >{title}</div>
             <div>views: {views}</div>
             <button onClick={()=> {document.querySelector('.popUpBackground').style = 'display: block;'}}>save to playlist </button>
+            </div>
+            </div>
+            </Link>
           </div>)
         })}
       </div>
     </div>
+  </div>
   )
 }
